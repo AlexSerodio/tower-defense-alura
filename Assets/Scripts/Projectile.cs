a178cc8dea675f9cd3b8e1381002051761edf83a
Unknown <alequis01@gmail.com>
2018-03-21 00:32:17 -0300

    Add tower and projectile instantiation

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	private float speed = 10f;
+
+	void Update () {
+		Walk();
+	}
+
+	private void Walk () {
+		transform.position += transform.forward * Time.deltaTime * speed; 
+	}
+}
---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TowerController : MonoBehaviour {
+
+	public GameObject projectilePrefab; 	
+	public float reloadTime = 1f;
+
+	private float lastShotTime;
+
+	void Update () {
+		Shoot();
+	}
+
+	private void Shoot () {
+		float currentTime = Time.time;
+		if (currentTime > lastShotTime + reloadTime) {
+			lastShotTime = currentTime;
+			Transform shooterPoint = this.transform.Find("TowerCannon/ShooterPoint");
+			Instantiate(projectilePrefab, shooterPoint.position, Quaternion.identity);
+		}
+	}
+}

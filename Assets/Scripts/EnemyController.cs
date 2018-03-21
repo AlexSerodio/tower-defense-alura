822ec8c11536a6672099f73719d7b54ae775e6ad
Unknown <alequis01@gmail.com>
2018-03-21 00:17:30 -0300

    Add enemy movement

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		GameObject endOfTheRoad = GameObject.Find("EndOfTheRoad");
+		agent.SetDestination (endOfTheRoad.transform.position);
+	}
+}

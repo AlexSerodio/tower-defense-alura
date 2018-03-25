c91c3d746fd1e79aa5f3721fe09d78df7aa37a08
Unknown <alequis01@gmail.com>
2018-03-24 23:26:46 -0300

    Add projectile damage and enemy health
    When the projectile collides with an enemy the enemy loses health and the projectile is destroyed. If enemy health is 0 or lower it is destroyed too.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,11 +3,22 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour {
 
+	[SerializeField]
+	private int health;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 
 		NavMeshAgent agent = GetComponent<NavMeshAgent>();
 		GameObject endOfTheRoad = GameObject.Find("EndOfTheRoad");
 		agent.SetDestination (endOfTheRoad.transform.position);
+	}
+
+	public void LoseHealth (int damage) 
+	{
+		health -= damage;
+		if (health <= 0)
+			Destroy(gameObject);
 	}
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,23 +2,49 @@
 
 public class Projectile : MonoBehaviour {
 
+	[SerializeField]
+	private int damage;
 	private float speed = 10f;
 	private GameObject target;
 
-	void Start() {
+	void Start() 
+	{
 		target = GameObject.Find("Enemy");
+		SelfDestroy(5f);
 	}
 
-	void Update () {
+	void Update () 
+	{
 		Walk();	
-		ChangeDirection();
+		if (target != null)
+			ChangeDirection();
 	}
 
-	private void Walk () {
+	private void Walk () 
+	{
 		transform.position += transform.forward * Time.deltaTime * speed; 
 	}
 
-	private void ChangeDirection () {
+	private void ChangeDirection () 
+	{
 		transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+	}
+
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Enemy")) {
+			Destroy(gameObject);
+			EnemyController enemy = other.GetComponent<EnemyController>();
+			enemy.LoseHealth(damage);
+		}
+	}
+
+	private void SelfDestroy (float time) 
+	{
+		Destroy(gameObject, time);
 	}
 }
